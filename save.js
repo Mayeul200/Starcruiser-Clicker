6c029871e7852b2439d6de29227801daa07c4583
Mayeul200 <mayeul.lacroix@orange.fr>
2026-08-24 20:05:23 +0200

    Update save.js

--- save.js
+++ save.js
@@ -1,1 +1,48 @@
+// Sauvegarde la partie dans le localStorage
+function saveGame() {
+    const saveData = {
+        score: score,
+        multipliers: multipliers,
+        eras: ERAS.map(era => ({
+            name: era.name,
+            requiredScore: era.requiredScore,
+            items: era.items.map(item => ({
+                id: item.id,
+                count: item.count,
+                globalUpgrade: item.globalUpgrade ? {
+                    unlocked: item.globalUpgrade.unlocked
+                } : null
+            }))
+        }))
+    };
+    localStorage.setItem('gloryOfFranceClicker', JSON.stringify(saveData));
+}
 
+// Charge la partie depuis le localStorage
+function loadGame() {
+    const saveData = localStorage.getItem('gloryOfFranceClicker');
+    if (saveData) {
+        try {
+            const parsedData = JSON.parse(saveData);
+            score = parsedData.score || 0;
+            multipliers = parsedData.multipliers || {};
+
+            if (parsedData.eras) {
+                parsedData.eras.forEach((savedEra, eraIndex) => {
+                    if (ERAS[eraIndex]) {
+                        savedEra.items.forEach((savedItem, itemIndex) => {
+                            if (ERAS[eraIndex].items[itemIndex]) {
+                                ERAS[eraIndex].items[itemIndex].count = savedItem.count || 0;
+                                if (ERAS[eraIndex].items[itemIndex].globalUpgrade && savedItem.globalUpgrade) {
+                                    ERAS[eraIndex].items[itemIndex].globalUpgrade.unlocked = savedItem.globalUpgrade.unlocked || false;
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        } catch (e) {
+            console.error("Erreur lors du chargement de la sauvegarde :", e);
+        }
+    }
+}
